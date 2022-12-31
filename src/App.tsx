eb39c14ec437b8a31075a072eec7bb91691531b6
choichoijin <cuj10594@yonsei.ac.kr>
2022-12-31 19:01:41 +0900

    design: setting basic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import macaron from "./macaron.png";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <StResultCard>
+        <header>유진님의 결과</header>
+        <h1>재충전이 필요한 마카롱</h1>
+        <img src={macaron} alt="마카롱" />
+      </StResultCard>
+      <button>다운로드</button>
+    </>
   );
 }
 
 export default App;
+
+const StResultCard = styled.div`
+  width: 500px;
+  height: 600px;
+  margin-top: 50px;
+  background-color: #fbf7ed;
+`;
