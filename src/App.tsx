15a8bb0aeb5a481424e9368faa66fd3fd882bea1
choichoijin <cuj10594@yonsei.ac.kr>
2022-12-31 19:32:01 +0900

    feat: make download button with feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import macaron from "./macaron.png";
 import styled from "styled-components";
+import html2canvas from "html2canvas";
+import { useRef } from "react";
 
 function App() {
+  const resultCard = useRef<HTMLDivElement>(null);
+  const onHtmlToPng = () => {
+    resultCard.current &&
+      html2canvas(resultCard.current).then((canvas) => {
+        onSaveAs(canvas.toDataURL("image/png"), "result.png");
+      });
+  };
+
+  const onSaveAs = (uri: string, filename: string) => {
+    const link = document.createElement("a");
+    link.href = uri;
+    link.download = filename;
+    link.click();
+  };
+
   return (
     <>
-      <StResultCard>
+      <StResultCard ref={resultCard}>
         <header>유진님의 결과</header>
         <h1>재충전이 필요한 마카롱</h1>
         <img src={macaron} alt="마카롱" />
       </StResultCard>
-      <button>다운로드</button>
+      <button onClick={onHtmlToPng}>다운로드</button>
     </>
   );
 }
@@ -18,8 +35,8 @@ function App() {
 export default App;
 
 const StResultCard = styled.div`
-  width: 500px;
-  height: 600px;
+  width: 300px;
+  height: 500px;
   margin-top: 50px;
   background-color: #fbf7ed;
 `;
