bd6a14ec916b6b6e3c7c916708db6263f5844265
choichoijin <cuj10594@yonsei.ac.kr>
2023-01-01 15:51:14 +0900

    feat: try again checking state of userId

--- src/pages/myResult.tsx
+++ src/pages/myResult.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import macaron from "./macaron.png";
 import styled from "styled-components";
 import html2canvas from "html2canvas";
 import { useRef } from "react";
-import { useRecoilValue } from "recoil";
-import { userIdState } from "../atom";
 
 function MyResult() {
-  const userId = useRecoilValue(userIdState);
+  useEffect(() => {
+    setUserId();
+  }, []);
+
+  const setUserId = () => {
+    localStorage.setItem("userId", "efgh");
+  };
+
   const resultCard = useRef<HTMLDivElement>(null);
   const onHtmlToPng = () => {
     resultCard.current &&
@@ -28,7 +33,6 @@ function MyResult() {
       <StResultCard ref={resultCard}>
         <header>유진님의 결과</header>
         <h1>재충전이 필요한 마카롱</h1>
-        <h2>유진님의 아이디는 {userId}입니다</h2>
         <img src={macaron} alt="마카롱" />
       </StResultCard>
       <button onClick={onHtmlToPng}>다운로드</button>
